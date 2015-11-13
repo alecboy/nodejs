bea681e9f892136ad3311c6336ae957d754e2499
alecboy <alejandro6230@gmail.com>
2015-11-13 19:03:40 +0100

    Initial commit

---
+++ chatMe/app.js
@@ -0,0 +1,50 @@
+var express = require('express');
+var app = express();
+
+var server = require('http').createServer(app);
+
+var io = require('socket.io').listen(server);
+
+if (process.env.PORT) {
+    server.listen(process.env.PORT);
+} else {
+    server.listen(3000);
+}
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '\\index.html');
+});
+
+var usernames = [];
+
+io.sockets.on('connection', function (socket) {
+    // New user
+    socket.on('new user', function (data, callback) {
+        if (usernames.indexOf(data) != -1) {
+            callback(false);
+        } else {
+            callback(true);
+            socket.username = data;
+            usernames.push(socket.username);
+            updateUsernames();
+            io.sockets.emit('new user', {user: data});
+        }
+    });
+
+    // Update usernames
+    function updateUsernames() {
+        io.sockets.emit('usernames', usernames);
+    }
+
+    // Send message event
+    socket.on('send message', function (data) {
+        io.sockets.emit('new message', {msg: data, user: socket.username});
+    });
+
+    // Disconect
+    socket.on('disconnect', function (data) {
+        if (!socket.username) return;
+        usernames.splice(usernames.indexOf(socket.username), 1);
+        updateUsernames();
+    });
+});

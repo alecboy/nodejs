4318c34cc6a01858408bb6d5250db32e3290d170
alecboy <alejandro6230@gmail.com>
2015-11-13 20:17:32 +0100

    Default port 3000

--- chatMe/app.js
+++ chatMe/app.js
@@ -5,7 +5,7 @@ var server = require('http').createServer(app);
 
 var io = require('socket.io').listen(server);
 
-server.listen(process.env.PORT);
+server.listen(process.env.PORT || 3000);
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
